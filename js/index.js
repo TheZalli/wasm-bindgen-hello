9d4fd65ca416e29f74bd94632cb42967543d0a8b
mrZalli <misterzalli@gmail.com>
2018-05-26 17:32:06 +0300

    Add DOM manipulation from wasm-bindgen examples

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ const rust = import("./../wasm_bindgen/wasm_bindgen_hello");
 
 rust.then(rust => {
   rust.greet("World");
+  rust.add_paragraph("Hello, World!");
 });
--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,33 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
+    type HTMLDocument;
+    static document: HTMLDocument;
+    #[wasm_bindgen(method)]
+    fn createElement(this: &HTMLDocument, tagName: &str) -> Element;
+    #[wasm_bindgen(method, getter)]
+    fn body(this: &HTMLDocument) -> Element;
+
+    type Element;
+    #[wasm_bindgen(method, setter = innerHTML)]
+    fn set_inner_html(this: &Element, html: &str);
+    #[wasm_bindgen(method, js_name = appendChild)]
+    fn append_child(this: &Element, other: Element);
+}
+
+#[wasm_bindgen]
+extern {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+}
+
+#[wasm_bindgen]
+pub fn add_paragraph(text: &str) {
+    let p = document.createElement("p");
+    p.set_inner_html(text);
+    document.body().append_child(p);
 }
